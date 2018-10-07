cbcaa059799e5896aad7d6d0b5857a174218a489
Aleksandr <ksandr.ua@gmail.com>
2018-10-07 08:25:52 +0300

    Added simple init Universe fill it and print

--- src/main/scala/com/aleksandrbogomolov/gameoflife/Main.scala
+++ src/main/scala/com/aleksandrbogomolov/gameoflife/Main.scala
@@ -2,5 +2,13 @@ package com.aleksandrbogomolov.gameoflife
 
 object Main extends App {
 
-  println("Started")
+  val width = 10
+  val height = 10
+
+  val uni = Universe(width, height)
+
+  for (x <- 0 until width;
+       y <- 0 until height) yield uni.structure(x)(y) = Cell(x, y, Alive())
+
+  uni.printStructure()
 }
