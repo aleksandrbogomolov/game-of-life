26474ee8a532f4677ed7e93cfbde2e0b90fab4d0
Aleksandr <ksandr.ua@gmail.com>
2018-10-07 08:20:38 +0300

    Added object Cell with apply

--- src/main/scala/com/aleksandrbogomolov/gameoflife/Cell.scala
+++ src/main/scala/com/aleksandrbogomolov/gameoflife/Cell.scala
@@ -1,3 +1,11 @@
 package com.aleksandrbogomolov.gameoflife
 
-case class Cell(x: Int, y: Int, state: State) {}
+class Cell(x: Int, y: Int, state: State) {
+
+  override def toString: String = s"x=$x y=$y state=$state"
+}
+
+object Cell {
+
+  def apply(x: Int, y: Int, state: State): Cell = new Cell(x, y, state)
+}
