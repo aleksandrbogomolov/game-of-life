9e403822dbc9895724d3fc15c3f019e76953babd
Aleksandr <ksandr.ua@gmail.com>
2018-10-07 14:34:19 +0300

    Changed toString in State

--- src/main/scala/com/aleksandrbogomolov/gameoflife/model/State.scala
+++ src/main/scala/com/aleksandrbogomolov/gameoflife/model/State.scala
@@ -4,10 +4,10 @@ sealed trait State {}
 
 case class Alive() extends State {
 
-  override def toString: String = "alive"
+  override def toString: String = "X"
 }
 
 case class Dead() extends State {
 
-  override def toString: String = "dead"
+  override def toString: String = " "
 }
